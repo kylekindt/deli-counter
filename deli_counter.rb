3e023745a1f0cdd1acd1c6449feee9a170ca510f
Kyle Kindt <k.kindt8@gmail.com>
2021-03-03 23:52:36 -0500

    commit tree 068adac390d89bfe691779f4e742c2e3dd03e333

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require "pry"
+def line(katz_deli)
+    new_line = "The line is currently:"
+    if katz_deli.empty?
+        puts "The line is currently empty."
+    else 
+        katz_deli.each_with_index do |people, index| 
+       new_line << " #{index + 1}. #{people}"
+    end
+puts new_line
+end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+
+end
+
+def now_serving(katz_deli)
+if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+else 
+    name = katz_deli.shift
+        puts "Currently serving #{name}."
+    end
+end
